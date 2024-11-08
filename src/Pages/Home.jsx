b043f101bdd0d02e17f6a3ad6b7e61434cb72094
Arun Ayothi <146436737+ArunDani002@users.noreply.github.com>
2024-11-08 22:02:32 +0530

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Home = () => {
+  return (
+    <>
+    <div>
+      <header>
+        <h1 className="Title">Village & Trible Mission</h1>
+      </header>
+      <div className='nav'>
+      </div>  
+      
+      <hr className='underline' />         
+    </div>
+    <div className='desc' >
+      <h2 className='motto'>Our Motto</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo itaque eligendi reiciendis maiores rem assumenda ipsa fugiat fugit. Obcaecati possimus, earum quod quisquam ipsum dolorem dignissimos rerum nihil provident hic aut, doloremque veritatis aliquam eum delectus. Blanditiis corporis enim magni tempore sint illo nihil hic ex atque commodi alias similique consequuntur, porro doloribus quidem amet. Magnam officia nobis cum, rem quam distinctio natus quibusdam. Enim tempora repellendus asperiores fuga vitae provident quae quod quidem maxime fugit quaerat tenetur, in sed hic, officiis consectetur facilis nisi quas ab? Expedita deserunt illum possimus explicabo suscipit illo molestiae error deleniti minima a inventore sunt enim recusandae ab quas quo quaerat dolorum aut maiores, excepturi at officiis necessitatibus! Aperiam inventore velit nam nulla numquam, non cupiditate commodi aut molestiae, iste quos placeat quasi perferendis repudiandae dolor vel vitae assumenda ullam odit veniam voluptate nesciunt! Quas placeat esse architecto dolorem. Sapiente voluptate soluta numquam rerum laboriosam voluptatibus? Provident, laudantium nemo enim consectetur ea rem, temporibus repellendus dolorem et neque rerum non saepe laboriosam voluptates amet eveniet a quo reprehenderit impedit optio? Cupiditate, dignissimos non corporis commodi laudantium voluptas nobis temporibus quia quos at architecto? Consequatur earum accusamus mollitia magni quam iste cumque, dignissimos officiis perspiciatis.</p>
+    </div>
+    <div className="circle">
+
+    </div>
+    <div className="circle1">
+      
+    </div>
+    <footer className='Footer'>
+      <h3>
+        Contact us
+      </h3>
+      <h1 >Village & Trible Mission</h1>
+      <span className='FooterAddress'>
+        <h3>Address</h3>
+        <address>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste mollitia quisquam veritatis harum commodi illo atque repudiandae, sed, qui eveniet tenetur! Voluptatem dolorum voluptas voluptatibus sed corrupti ut reiciendis provident.
+        </address>
+      </span>
+
+
+    </footer>
+
+  </>
+
+  )
+}
+
+export default Home
