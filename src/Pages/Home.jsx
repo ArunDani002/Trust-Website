b3ce2951fdf58c55f3c5941fd201c0fffac553cd
Arun <arundani002@gmail.com>
2024-11-09 23:27:58 +0530

    Styling Changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,12 +17,72 @@ const Home = () => {
       <h2 className='motto'>Our Motto</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo itaque eligendi reiciendis maiores rem assumenda ipsa fugiat fugit. Obcaecati possimus, earum quod quisquam ipsum dolorem dignissimos rerum nihil provident hic aut, doloremque veritatis aliquam eum delectus. Blanditiis corporis enim magni tempore sint illo nihil hic ex atque commodi alias similique consequuntur, porro doloribus quidem amet. Magnam officia nobis cum, rem quam distinctio natus quibusdam. Enim tempora repellendus asperiores fuga vitae provident quae quod quidem maxime fugit quaerat tenetur, in sed hic, officiis consectetur facilis nisi quas ab? Expedita deserunt illum possimus explicabo suscipit illo molestiae error deleniti minima a inventore sunt enim recusandae ab quas quo quaerat dolorum aut maiores, excepturi at officiis necessitatibus! Aperiam inventore velit nam nulla numquam, non cupiditate commodi aut molestiae, iste quos placeat quasi perferendis repudiandae dolor vel vitae assumenda ullam odit veniam voluptate nesciunt! Quas placeat esse architecto dolorem. Sapiente voluptate soluta numquam rerum laboriosam voluptatibus? Provident, laudantium nemo enim consectetur ea rem, temporibus repellendus dolorem et neque rerum non saepe laboriosam voluptates amet eveniet a quo reprehenderit impedit optio? Cupiditate, dignissimos non corporis commodi laudantium voluptas nobis temporibus quia quos at architecto? Consequatur earum accusamus mollitia magni quam iste cumque, dignissimos officiis perspiciatis.</p>
     </div>
+    <div>
+      
+    </div>
     <div className="circle">
 
     </div>
-    <div className="circle1">
-      
+    <hr/>
+    <div
+    style={{
+      display:'flex',
+      flexDirection:'column',
+      textAlign:'center',
+      alignItems:'center',
+      justifyContent:'center',
+      marginRight:400,
+      marginLeft:400,
+    }}>
+      <h2 style={{textAlign:'center'}}>What we are doing?</h2>
+      <p style={{textAlign:'center'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aliquam repellat consectetur voluptatum commodi porro perferendis? Ea eaque eligendi aperiam facilis blanditiis reiciendis similique necessitatibus nostrum labore consequuntur! Facilis, consectetur.</p>
+      <div style={{
+        display:'flex',       
+
+      }}>
+        <div
+          style={{
+            border:'1px solid',
+            borderColor:'black',
+            width:300,
+            height:400,
+            margin:10
+            
+          }}
+        >
+        </div>
+        <div
+          style={{
+            border:'1px solid',
+            borderColor:'black',
+            width:300,
+            height:400,
+            margin:10
+          }}
+        >
+        </div>
+        <div
+          style={{
+            border:'1px solid',
+            borderColor:'black',
+            width:300,
+            height:400,
+            margin:10
+          }}
+        >
+        </div>
+        
+
+      </div>
+
+    
     </div>
+    <br />
+    <br />
+    <hr/>
+    <div className="circle1">
+    </div>
+    <br />
     <footer className='Footer'>
       <h3>
         Contact us
